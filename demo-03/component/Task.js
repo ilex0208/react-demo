8c13130e0ee558e38af38abdc1b0b7e28874c7f7
ilex0208 <tiangonglei@126.com>
2016-06-08 17:35:34 +0800

    Create Task.js

---
+++ demo-03/component/Task.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Task extends Component {
+    render() {
+        return (
+            <div>
+                Task-->任务管理页面
+            </div>
+        );
+    }
+}
+
+export default Task;
