525783d01174886007ffa2ce2ded944b2c57df38
ilex0208 <tiangonglei@126.com>
2016-06-08 17:29:15 +0800

    Create webpack.config.js

---
+++ demo-02/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: {
+      app:'./App.js'
+  },
+
+  output: {
+    publicPath: '/build/', //服务器根路径
+    path: './build', //编译到当前目录
+    filename: 'bundle.js' //编译后的文件名字
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  }
+}
