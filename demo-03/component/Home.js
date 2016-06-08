787cfb2806ba09e663f38bd15441abda2e678036
ilex0208 <tiangonglei@126.com>
2016-06-08 17:31:39 +0800

    Create Home.js

---
+++ demo-03/component/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                Home-->主页
+            </div>
+        );
+    }
+}
+
+export default Home;
